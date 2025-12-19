f00ec44ba7573839ea6fecc8d559248af07c6dd2
anandajuliar <anandajulia001@gmail.com>
2025-12-19 20:19:19 +0700

    membuat destinasi entry

--- app/src/main/java/com/example/questapi_046/uicontroller/route/DestinasiEntry.kt
+++ app/src/main/java/com/example/questapi_046/uicontroller/route/DestinasiEntry.kt
@@ -1,4 +1,9 @@
 package com.example.questapi_046.uicontroller.route
 
-object DestinasiEntry {
+import com.example.questapi_046.R
+
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = R.string.entry_siswa
 }
