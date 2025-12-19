96a40f00053c1bdc47c2680cff7d087ff73ba0af
anandajuliar <anandajulia001@gmail.com>
2025-12-19 18:08:02 +0700

    menambahkan service api

--- app/src/main/java/com/example/questapi_046/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_046/apiservice/ServiceApiSiswa.kt
@@ -1,4 +1,16 @@
 package com.example.questapi_046.apiservice
 
+import com.example.questapi_046.modeldata.DataSiswa
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface ServiceApiSiswa {
+    @GET(value = "bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+    @POST(value = "insertTM.php")
+    suspend fun postSiswa (@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
 }
+
+
+
