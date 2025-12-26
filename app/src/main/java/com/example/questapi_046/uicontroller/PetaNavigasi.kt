1731de275150658f48999ee39133c87cd9aaaebe
anandajuliar <anandajulia001@gmail.com>
2025-12-26 20:41:20 +0700

    menmabah composable untuk destinasi detail 2

--- app/src/main/java/com/example/questapi_046/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_046/uicontroller/PetaNavigasi.kt
@@ -41,5 +41,11 @@ fun HostNavigasi(
             route = DestinasiDetail.routeWithArgs,
             arguments = listOf(navArgument(DestinasiDetail.itemIdArg) { type = NavType.IntType })
         ) {
+            DetailSiswaScreen(
+                navigateToEditItem = { navController.navigate("${DestinasiEdit.route}/$it") },
+                navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+
     }
 }
