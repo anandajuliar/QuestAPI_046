3148ba2ca1030d01a050fbc845ca1efd9dd73090
anandajuliar <anandajulia001@gmail.com>
2025-12-26 20:21:21 +0700

    manggil services api getstatusiswa

--- app/src/main/java/com/example/questapi_046/repositori/RepositoryDataSiswa.kt
+++ app/src/main/java/com/example/questapi_046/repositori/RepositoryDataSiswa.kt
@@ -24,4 +24,6 @@ class JaringanRepositoryDataSiswa(
     override suspend fun postDataSiswa(dataSiswa: DataSiswa): Response<Void> {
         return serviceApiSiswa.postSiswa(dataSiswa)
     }
+
+    override suspend fun getSatuSiswa(id: Int): DataSiswa = serviceApiSiswa.getSatuSiswa(id)
 }
