975f165bf7c86923c8b83168b4fae3e86ecf752b
anandajuliar <anandajulia001@gmail.com>
2025-12-26 19:51:04 +0700

    add fungsi buat mengambil data 1 siswa dari server api

--- app/src/main/java/com/example/questapi_046/apiservice/ServiceApiSiswa.kt
+++ app/src/main/java/com/example/questapi_046/apiservice/ServiceApiSiswa.kt
@@ -2,14 +2,21 @@ package com.example.questapi_046.apiservice
 
 import com.example.questapi_046.modeldata.DataSiswa
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 interface ServiceApiSiswa {
     @GET(value = "bacaTeman.php")
     suspend fun getSiswa(): List<DataSiswa>
     @POST(value = "insertTM.php")
     suspend fun postSiswa (@Body dataSiswa: DataSiswa): retrofit2.Response<Void>
+
+    @GET("baca1Teman.php/{id}")
+    suspend fun getSatuSiswa(@Query("id") id: Int): DataSiswa
+
 }
 
 
